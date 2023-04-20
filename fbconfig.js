2de4346a2267906488b86ce1812b50728b1562f1
Caetanocc <58012552+Caetanocc@users.noreply.github.com>
2023-04-20 20:11:10 -0300

    fbconfig.js Vazio.

---
+++ fbconfig.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+    };
+  
+    // Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+  
+    // Firebase Database Reference and the child
+const dbRef = firebase.database().ref();
+const pessoaRef = dbRef.child('pessoas');
+  
